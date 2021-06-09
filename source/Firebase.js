268b35d22b8fc64b56cdf116f8aaca81610ac5e4
Juan David Payán <JuanDavidPF@gmail.com>
2021-06-09 14:57:54 -0500

    Project Creation

---
+++ source/Firebase.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDE5R8jOUUEAHM6-X5KNYHQipPvO0EDC9U",
+  authDomain: "sti-cc3a2.firebaseapp.com",
+  projectId: "sti-cc3a2",
+  storageBucket: "sti-cc3a2.appspot.com",
+  messagingSenderId: "674933075620",
+  appId: "1:674933075620:web:69a981ba00f261c5b30031"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebase.firestore();
+
+
+
---
+++ source/Router.js
@@ -0,0 +1,47 @@
+//link
+let baseLink = "";
+
+function AnalyzeLink() {
+  let link = window.location.href;
+  let linkRoute = "";
+
+  if (!link.includes("#")) {
+    baseLink = link + "#";
+    window.location.href = baseLink;
+  } else {
+    let linkSegments = [];
+    linkSegments = link.split("#");
+    baseLink = linkSegments[0] + "#";
+    linkRoute = linkSegments[1];
+  }
+  return linkRoute;
+} //closes Analyzelink method;
+
+function Router(path) {
+  let linkRoute = AnalyzeLink();
+  switch (linkRoute) {
+    case "":
+      break;
+
+    case "/404":
+   
+      break;
+    default:
+      Redirect("/404");
+      break;
+  } //closes Router switch
+} //closes Router method
+
+function Redirect(path) {
+  window.location.href = baseLink + path;
+} //closes Redirect method
+
+function Refresh() {
+  Router();
+} //closes Redirect method
+
+window.onhashchange = function () {
+  Router();
+}; //closes onHasChange event
+
+Router();
